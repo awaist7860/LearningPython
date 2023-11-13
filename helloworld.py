e0526eaf3015d772c267bdbb919ca99be4956697
Awais Tasleem <48568803+awaist7860@users.noreply.github.com>
2023-11-13 18:55:17 +0000

    Upto Variables done
    Tutorial upto python variables has been done

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,97 @@
 print("Hello World")
+
+if 5 > 2:
+    print("5 is greater than 2")
+    print("This is a 1 indent")
+if 10 > 5:
+        print("10 is big")
+        print("This is a 2 indent")
+
+x = 20  #This is 20
+y = "Hello World"   #this is hello world string
+
+if x > 5:   #compares x to 5
+      #this will be called is x is bigger than 5
+      print("X = 20 and is bigger than 5")  
+      print(y)
+
+
+#Variables
+#Variables are case sensative
+#A and a are 2 different variables
+#A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
+#A variable name cannot be any of the Python keywords.
+#declaring
+a = 5
+b = "Awais"
+print(a)
+print(b)
+
+
+c = 4   #c is of type int
+print(c)
+c = "Tasleem"   #d is of type string
+print(c)
+
+#type casting
+
+t = str(3)      #t will be '3'
+h = int(3)      #h will be 3
+q = float(3)    #q will be 3.0
+
+print(t)
+print(type(t))  #getting variable type
+print(h)
+print(type(h))  #getting variable type
+print(q)
+print(type(q))  #getting variable type
+
+#single and double quotes "" or ''
+#They both can be used
+
+name1 = "Awais"
+name2 = 'Tasleem'
+
+print(name1 + ' ' + name2)
+
+#Assigning multiple values to mutliple variables
+w,e,r = "Oranges", "Banana", "Apple"
+
+print(w)
+print(e)
+print(r)
+
+#assigning 1 value to multiple variables
+u = i = o = "Cherry"
+
+print(u)
+print(i)
+print(o)
+
+#unpacking a collection
+fruit = ["Audi", "Volvo", "BMW"]
+
+m,n,v = fruit
+
+print(m)
+print(n)
+print(v)
+
+#functions
+#Globals variables
+
+#p = "Global varibale Function/Method"
+
+def myFunc():
+      #print("This is a function and this is p is a varible from outside the function " + p)
+      global p
+      p = "Global variable"
+      print(p)
+
+myFunc()
+
+print(p)
