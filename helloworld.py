4852fb710569c2970dd899ac1eaf62064bad8328
Awais Tasleem <48568803+awaist7860@users.noreply.github.com>
2023-11-16 21:20:45 +0000

    Getting Keys and Values

--- helloworld.py
+++ helloworld.py
@@ -1730,6 +1730,77 @@ x = thisdict.get("model")
 discX = thisdict.get("year")
 print(discX)
 
+#Getting the Keys
+#The keys() method will return a list of all the keys in the dictionary.
+
+#Example
+keyX = thisdict.keys()
+print(keyX)
+
+#The list of the keys is a view of the dictionary, meaning that any changes done to the dictionary will be reflected in the keys list.
+
+"""
+Example
+Add a new item to the original dictionary, and see that the keys list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
+"""
+
+
+#Getting Values
+#The values() method will return a list of all the values in the dictionary.
+
+valX = thisdict.values()
+print(valX)
+
+"""
+***Example 1***
+Make a change in the original dictionary, and see that the values list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change
+
+***Example 2***
+Add a new item to the original dictionary, and see that the values list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.values()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+"""
+
 """
 class myclass():
   def __len__(self):
