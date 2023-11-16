474472a7f8730d4624b751bee3b62a3755f3cdf0
Awais Tasleem <48568803+awaist7860@users.noreply.github.com>
2023-11-16 21:11:17 +0000

    First part of Dictionaries done

--- helloworld.py
+++ helloworld.py
@@ -1587,9 +1587,117 @@ thisdict = {
 print(thisdict)
 
 """
+#example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(thisdict)
+
+"""
+***Dictionary Items***
+Dictionary items are ordered, changeable, and does not allow duplicates.
+
+Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+
+Example
+Print the "brand" value of the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+"""
+
+#exmaple of using a key
+print(thisdict["model"])
+
+"""
+***Ordered or Unordered?***
+As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+
+When we say that dictionaries are ordered, it means that the items have a defined order, and that order will not change.
+
+Unordered means that the items does not have a defined order, you cannot refer to an item by using an index.
+
+***Changeable***
+Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+
+***Duplicates Not Allowed***
+Dictionaries cannot have two items with the same key:
+
+Example
+Duplicate values will overwrite existing values:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
 
 
+***Dictionary Length***
+To determine how many items a dictionary has, use the len() function:
 
+Example
+Print the number of items in the dictionary:
+
+print(len(thisdict))
+
+"""
+
+#getting length of dictionary
+print(len(thisdict))
+
+"""
+***Dictionary Items - Data Types***
+The values in dictionary items can be of any data type:
+
+Example
+String, int, boolean, and list data types:
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+***type()***
+From Python's perspective, dictionaries are defined as objects with the data type 'dict':
+
+<class 'dict'>
+Example
+Print the data type of a dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+"""
+
+print(type(thisdict))
+
+"""
+***The dict() Constructor***
+It is also possible to use the dict() constructor to make a dictionary.
+
+Example
+Using the dict() method to make a dictionary:
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+"""
 
 """
 class myclass():
