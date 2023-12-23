59dbd49df55e0fca68c2cbfe225a0b35280e9a16
Awais Tasleem <48568803+awaist7860@users.noreply.github.com>
2023-12-23 13:29:32 +0000

    Functions sections finished
    Starting on lamddas now

--- helloworld.py
+++ helloworld.py
@@ -2819,6 +2819,340 @@ def my_function():
 
 """
 
+def new_Func():
+      print("Hello World from function")
+
+
+"""
+
+Calling a Function
+To call a function, use the function name followed by parenthesis:
+
+Example
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+"""
+
+
+new_Func()
+
+"""
+
+Arguments
+Information can be passed into functions as arguments.
+
+Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+
+***Arguments are often shortened to args in Python documentations.***
+
+The following example has a function with one argument (fname). When the function is called, we pass along a first name, which is used inside the function to print the full name:
+
+Example
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+"""
+
+def nameFunc(newName):
+      print(newName + "Random stuff")
+
+
+nameFunc("Awais ")
+nameFunc("Hamzah ")
+
+
+"""
+
+Parameters or Arguments?
+The terms parameter and argument can be used for the same thing: information that are passed into a function.
+
+From a function's perspective:
+
+A parameter is the variable listed inside the parentheses in the function definition.
+
+An argument is the value that is sent to the function when it is called.
+
+
+
+Number of Arguments
+By default, a function must be called with the correct number of arguments. Meaning that if your function expects 2 arguments, you have to call the function with 2 arguments, not more, and not less.
+
+Example
+This function expects 2 arguments, and gets 2 arguments:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+If you try to call the function with 1 or 3 arguments, you will get an error:
+Example
+This function expects 2 arguments, but gets only 1:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")
+
+
+"""
+
+def argsTest(test1, test2):
+      print(test1 + " " + test2)
+
+
+argsTest("Test 1", "Test 2")
+
+
+"""
+
+Arbitrary Arguments, *args
+If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+
+This way the function will receive a tuple of arguments, and can access the items accordingly:
+
+Example
+If the number of arguments is unknown, add a * before the parameter name:
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+***Arbitrary Arguments are often shortened to *args in Python documentations.***
+
+"""
+
+def argsNotKnown(*nk):
+      print("This a test for arbitary arguments " + nk[2])
+
+argsNotKnown("No1", "No2", "No3", "No4")
+
+
+"""
+
+Keyword Arguments
+You can also send arguments with the key = value syntax.
+
+This way the order of the arguments does not matter.
+
+***The phrase Keyword Arguments are often shortened to kwargs in Python documentations.***
+
+Example
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+"""
+
+def keyWordArgs(check2, check3, check1):
+      print(check1, check2, check3)
+
+keyWordArgs(check1="Awais 1", check2="Awais 2", check3="Awais 3")
+
+
+"""
+
+Arbitrary Keyword Arguments, **kwargs
+If you do not know how many keyword arguments that will be passed into your function, add two asterisk: ** before the parameter name in the function definition.
+
+This way the function will receive a dictionary of arguments, and can access the items accordingly:
+
+Example
+If the number of keyword arguments is unknown, add a double ** before the parameter name:
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+
+***Arbitrary Kword Arguments are often shortened to **kwargs in Python documentations.***
+
+"""
+
+def myNameis(**nameStar):
+      print("my name is " + nameStar["starName"])
+  
+myNameis(no1Name = "Awais", starName="Hamzah")
+
+
+"""
+
+Default Parameter Value (Constructors)
+The following example shows how to use a default parameter value.
+
+If we call the function without argument, it uses the default value:
+
+Example
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+"""
+
+def countryName(countryN = "Pakistan"):
+      print("I am from " + countryN)
+
+countryName()
+countryName("England")
+countryName("UK")
+
+
+"""
+
+Passing a List as an Argument
+You can send any data types of argument to a function (string, number, list, dictionary etc.), and it will be treated as the same data type inside the function.
+
+E.g. if you send a List as an argument, it will still be a List when it reaches the function:
+
+Example
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+"""
+
+def fruitFunction(foodFunc):
+      for x in foodFunc:
+            print(x)
+
+
+fruitFunction(fruitsList)
+
+
+"""
+
+Return Values
+To let a function return a value, use the return statement:
+
+Example
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+"""
+
+def returnFunc(nReturn):
+      return 5 * nReturn
+
+
+print(returnFunc(5))
+print(returnFunc(9))
+print(returnFunc(8))
+print(returnFunc(4))
+print(returnFunc(2))
+print(returnFunc(7))
+print(returnFunc(15))
+
+
+"""
+
+The pass Statement
+function definitions cannot be empty, but if you for some reason have a function definition with no content, put in the pass statement to avoid getting an error.
+
+Example
+def myfunction():
+  pass
+
+"""
+
+"""
+Need to come back to this
+***Recursion***
+Python also accepts function recursion, which means a defined function can call itself.
+
+Recursion is a common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result.
+
+The developer should be very careful with recursion as it can be quite easy to slip into writing a function which never terminates, or one that uses excess amounts of memory or processor power. However, when written correctly recursion can be a very efficient and mathematically-elegant approach to programming.
+
+In this example, tri_recursion() is a function that we have defined to call itself ("recurse"). We use the k variable as the data, which decrements (-1) every time we recurse. The recursion ends when the condition is not greater than 0 (i.e. when it is 0).
+
+To a new developer it can take some time to work out how exactly this works, best way to find out is by testing and modifying it.
+
+Example
+Recursion Example
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results")
+tri_recursion(6)
+
+"""
+
+def trying_recursion(rk):
+      if(rk > 0):                                         #break point need here
+            result = rk + trying_recursion(rk - 1)        #break point need here
+            print(result)                                 #break point need here
+      else:                                               #break point need here
+            result = 0                                    #break point need here
+      return result                                       #break point need here
+
+print("This is going to call the trying_recursion function")
+print("It will be called 10 time or until it reaches 0")
+
+trying_recursion(10)  #outputs 1, 3, 6, 10, 15, 21, 28, 36, 45, 55
+
+
+"""
+
+Python Lambda
+
+A lambda function is a small anonymous function.
+
+A lambda function can take any number of arguments, but can only have one expression.
+
+Syntax
+lambda arguments : expression
+
+
+The expression is executed and the result is returned:
+
+Example
+
+Add 10 to argument a, and return the result:
+
+x = lambda a : a + 10
+print(x(5))
+
+"""
+
+
+
+
+
+
+
+
+
+
+
 
 
 
