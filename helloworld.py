db525c1da6fcbac5715536fb0d04b4619269673f
Awais Tasleem <48568803+awaist7860@users.noreply.github.com>
2023-12-24 00:29:17 +0000

    Classes and objects done
    Moving onto inheritance

--- helloworld.py
+++ helloworld.py
@@ -3311,26 +3311,41 @@ sort()	Sorts the list
 
 """
 
+"""
 
+Python Classes and Objects
 
+Python Classes/Objects
 
+Python is an object oriented programming language.
 
+Almost everything in Python is an object, with its properties and methods.
 
+A Class is like an object constructor, or a "blueprint" for creating objects.
 
+Create a Class
+To create a class, use the keyword class:
 
+Example
 
+Create a class named MyClass, with a property named x:
 
-
-
-
-
-
-
-
-
-
+class MyClass:
+  x = 5
 
 """
+
+
+
+
+class newClass():
+      
+      x = 5
+
+c1 = newClass()
+print(c1.x)
+print("This is from newClass()")
+
 class myclass():
   def __len__(self):
     return 0
@@ -3348,4 +3363,157 @@ if myFunc():
 else:
      print("myFunc returned False")
 
+
+
 """
+
+The __init__() Function (Like Class Constructor Method)
+The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+
+To understand the meaning of classes we have to understand the built-in __init__() function.
+
+All classes have a function called __init__(), which is always executed when the class is being initiated.
+
+Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+
+Example
+Create a class named Person, use the __init__() function to assign values for name and age:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+***Note: The __init__() function is called automatically every time the class is being used to create a new object.***
+
+"""
+
+class Person:
+      
+      def __init__(self, name, age):
+            self.name = name
+            self.age = age
+
+      def __str__(self):
+            return f"{self.name}({self.age})"
+
+          
+person1 = Person("Awais", 23)
+
+print(person1.name, person1.age)
+
+
+"""
+
+The __str__() Function
+The __str__() function controls what should be returned when the class object is represented as a string.
+
+If the __str__() function is not set, the string representation of the object is returned:
+
+Example
+The string representation of an object WITHOUT the __str__() function:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+
+Example
+The string representation of an object WITH the __str__() function:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+***Note: The self parameter is a reference to the current instance of the class, and is used to access variables that belong to the class.***
+
+The self Parameter
+The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+
+It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
+
+Example
+Use the words mysillyobject and abc instead of self:
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+"""
+
+print(person1)  #gives the name of the object and memory location wen no __str__ method is set
+print(person1)   #returns Awais(23)
+
+
+class Person2:
+      
+      def __init__(self, name, age):
+            self.name = name
+            self.age = age
+
+      def p2Func(self):
+            print("Hello, my name " + self.name)
+
+person2 = Person2("Awais Tasleem", 23)
+
+person2.p2Func()
+
+"""
+
+Modify Object Properties
+You can modify properties on objects like this:
+
+Example
+Set the age of p1 to 40:
+
+p1.age = 40
+
+"""
+
+person2.age = 21
+print(person2.age)
+
+"""
+
+Delete Object Properties
+You can delete properties on objects by using the del keyword:
+
+Example
+Delete the age property from the p1 object:
+
+del p1.age
+
+Delete Objects
+You can delete objects by using the del keyword:
+
+Example
+Delete the p1 object:
+
+del p1
+
+"""
+
