36c0866890a1cfd7a3fab8d30034dccb287d27b3
Awais Tasleem <48568803+awaist7860@users.noreply.github.com>
2023-12-24 11:18:41 +0000

    Inheritance Finished
    Starting iterators.

--- helloworld.py
+++ helloworld.py
@@ -3537,3 +3537,151 @@ Parent class is the class being inherited from, also called base class.
 Child class is the class that inherits from another class, also called derived class.
 
 """
+
+#parent class
+class ParentPerson:
+      def __init__(self, parentFname, parnetLname):
+            self.classFirstName = parentFname
+            self.classLastName = parnetLname
+          
+      def printName(self):
+            print(self.classFirstName, self.classLastName)
+  
+newX = ParentPerson("Awais", "Tasleem")
+
+newX.printName()
+
+
+"""
+
+Create a Child Class
+To create a class that inherits the functionality from another class, send the parent class as a parameter when creating the child class:
+
+Example
+Create a class named Student, which will inherit the properties and methods from the Person class:
+
+class Student(Person):
+  pass
+  
+***Note: Use the pass keyword when you do not want to add any other properties or methods to the class.***
+
+"""
+
+#child class
+            
+
+
+
+"""
+
+Now the Student class has the same properties and methods as the Person class.
+
+Example
+Use the Student class to create an object, and then execute the printname method:
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+"""
+
+
+
+
+"""
+
+Add the __init__() Function
+So far we have created a child class that inherits the properties and methods from its parent.
+
+We want to add the __init__() function to the child class (instead of the pass keyword).
+
+Note: The __init__() function is called automatically every time the class is being used to create a new object.
+
+When you add the __init__() function, the child class will no longer inherit the parent's __init__() function.
+
+Note: The child's __init__() function overrides the inheritance of the parent's __init__() function.
+
+To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+
+Example
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+Now we have successfully added the __init__() function, and kept the inheritance of the parent class, and we are ready to add functionality in the __init__() function.
+
+Use the super() Function
+Python also has a super() function that will make the child class inherit all the methods and properties from its parent:
+
+Example
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+
+By using the super() function, you do not have to use the name of the parent element, it will automatically inherit the methods and properties from its parent.
+
+
+
+Add Properties
+Example
+Add a property called graduationyear to the Student class:
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+In the example above, the year 2019 should be a variable, and passed into the Student class when creating student objects. To do so, add another parameter in the __init__() function:
+
+If you add a method in the child class with the same name as a function in the parent class, the inheritance of the parent method will be overridden.
+    
+"""
+
+
+class ChildPerson(ParentPerson):
+      
+      def __init__(self, childFName, childSName, childGradYear):
+            #self.childClassFName = childFName
+            #self.childClassSName = childSName
+            #ParentPerson.__init__(self, childFName, childSName)
+            super().__init__(childFName, childSName)
+            self.graduationYear = childGradYear
+
+      def welcome(self):
+            print("Welcome ", self.classFirstName, self.classLastName, self.graduationYear)
+
+
+
+childX = ChildPerson("Hamzah", "Tasleem", 2023)
+childX.welcome()
+
+
+"""
+
+Python Iterators
+
+Python Iterators
+
+An iterator is an object that contains a countable number of values.
+
+An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+
+Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
+
+Iterator vs Iterable
+Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+
+All these objects have a iter() method which is used to get an iterator:
+
+Example
+
+Return an iterator from a tuple, and print each value:
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+"""
